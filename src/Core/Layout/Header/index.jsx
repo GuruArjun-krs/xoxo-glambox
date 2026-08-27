62b5af4db6fc1818ce3df5ecf3b694d8a089fdbf
Arjun-krs <guruarjunkrs@gmail.com>
2026-08-27 10:29:08 +0530

    header fixed

--- src/Core/Layout/Header/index.jsx
+++ src/Core/Layout/Header/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Logo from '../../../Assets/Images/ceba-portfolio-logo.png';
+import './style.scss'
 
 const NavigationOptions = [
   {
@@ -32,15 +33,15 @@ const Header = () => {
   }, []);
 
   return (
-    <div className={`fixed top-0 w-full z-50 h-[70px] flex justify-center items-center transition-all duration-300 ${isScrolled ? 'bg-black shadow-md' : 'bg-transparent'}`}>
+    <div className={`fixed top-0 w-full z-50 h-[70px] flex justify-center items-center transition-all duration-300 bg-transparent`}>
       <div className="container relative flex items-center justify-between h-full">
-        <a href="#main_wrapper" className="absolute left-[-75px] top-[-20px] w-[250px] h-[110px]">
+        <a href="#main_wrapper" className="absolute left-[-75px] top-0 w-[250px] h-[110px]">
           <img src={Logo} alt="Logo" className="w-full h-full object-cover" />
         </a>
 
         <nav className="ml-auto hidden md:flex items-center gap-8">
           {NavigationOptions.map((el) => (
-            <a key={el.id} href={el.id} className="span-regular text-white hover:text-primary-500 transition-colors">
+            <a key={el.id} href={el.id} className={`${isScrolled ? 'nav_glasseffect_wrapper' : ''} px-4 py-1 span-regular text-white hover:text-primary-500 transition-colors`}>
               {el.title}
             </a>
           ))}
